86620cefe3e8c03cb2dbbab128c013a8f0be42cb
EMT95 <eze-telias@hotmail.com>
2017-06-03 15:36:49 -0300

    Se Agrego la clase Main -> AlgoBall

---
+++ src/AlgoBall.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AlgoBall extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("AlgoBall");
+
+    }
+}
