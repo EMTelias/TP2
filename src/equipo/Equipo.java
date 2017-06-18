eb682e25a8ba2818d8f4de517dfdb3001e412df5
EMT95 <eze-telias@hotmail.com>
2017-06-17 23:56:43 -0300

    Se eliminan los archivos de compilacion

---
+++ src/equipo/Equipo.java
@@ -0,0 +1,38 @@
+package equipo;
+
+
+import personaje.Personaje;
+
+import java.util.ArrayList;
+
+public class Equipo {
+    private final String nombre;
+    private ArrayList<Personaje> personajes;
+    protected boolean suTurno;
+
+
+    public Equipo(String nombre) {
+        this.nombre = nombre;
+        this.personajes = new ArrayList<Personaje>();
+    }
+    
+    public void agregarPersonaje(Personaje unPersonaje) {
+        this.personajes.add(unPersonaje);
+    }
+
+    public boolean esMiembro(Personaje otroPersonaje) {
+        return this.personajes.contains(otroPersonaje);
+    }
+
+    public ArrayList<Personaje> getPersonajes() {
+        return this.personajes;
+    }
+
+    public void setTurno(boolean esSuTurno) {
+        this.suTurno = esSuTurno;
+    }
+
+    public boolean getTurno() {
+        return this.suTurno;
+    }
+}
